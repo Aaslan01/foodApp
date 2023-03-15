b43b63dc9364dd097b2919267ec1b0562d2cbc15
Ali Sher Khan <111129437+alisherkhan01@users.noreply.github.com>
2023-03-15 22:34:51 +0500

    🔦 Navigation added & link created

---
+++ constants/StyleGuide.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+const white = '#fff';
+const black = '#000';
+const dark = '#1E1E1E';
+const green = '#89B601';
+const gray = '#9A9A9A';
+const yellow = '#FFD747';
+
+const StyleGuide = {
+  colors: {
+    darkText: black,
+    text: dark,
+    background: white,
+    primary: green,
+    onPrimary: white,
+    border: gray,
+    borderWithOpacity: 'rgba(154,154,154,0.45)',
+    secondary: yellow,
+    gray: gray,
+  },
+  
+};
+
+export default StyleGuide;
---
+++ screens/OpeningScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const OpeningScreen = () => {
+  return (
+    <View>
+      <Text style={{color: 'black', fontSize: 100}}>OpeningScreen</Text>
+    </View>
+  );
+};
+
+export default OpeningScreen;
